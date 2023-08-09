0e8997c3e94bbf90553495beb19dab1bca0bb185
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-08-09 20:16:49 +0500

    Fix Linters Error

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,12 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const root = document.getElementById('root');
+const app = (
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+ReactDOM.createRoot(root).render(app);
