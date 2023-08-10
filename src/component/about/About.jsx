5d85e65eb72f1252a8e6604341107232db0f20f2
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-08-10 22:12:24 +0500

    Update About Component

--- src/component/about/About.jsx
+++ src/component/about/About.jsx
@@ -12,30 +12,16 @@ const About = () => {
     setIsFullContentVisible(!isFullContentVisible);
   };
 
-  const content = `My name is Mohmmad Rafi Amin.
-    I have always been enthusiastic about programming from a very young age, making me curious and always eager to learn. Before hearing about Microverse, I was a self-taught software developer through consistent online research and also learning through some paid Udemy courses I spend most of my time building web applications and learning about the design and architecture of new technologies. I succeed in fast-paced environments as I love simple and elegant solutions to problems...
-  
-    I have a BSc. Computer Science. And 1 years+ of experience as a software engineer.
-    I am goal-oriented and always focused on reaching or completing specific tasks to achieve a planned outcome. Working with a partner or a team is a quality I have, and my teammates have also commended that during the level-up program. Being punctual and always eager to abide by the code of conduct that guides an organization is something I do effortlessly. I also take corrections to improve myself better.
-
-    Tech Stack:
-    • Languages: JavaScript, Typescript. HTML, CSS, Ruby, Python, Go, Shell
-    • Frameworks & Libraries: React, Next, Redux, Node, Rails, Bootstrap, Tailwind, Bulma
-    • Databases: PSQL, MySQL (learning MongoDB and firebase)
-    • Management & Deployment: Git, GitHub, Jira, Heroku, Netlify, Postman, Terminal
-    • Tools & Methods: RSpec, Jest, TDD, Dev Tools, Gitflow
-
-    Tools & Platforms: Git, Webpack, Jest, RSpec, Netlify, Heroku, Vercel, Test-Driven development, Mobile/Responsive Design, Agile methodologies
-
-    Other: Remote Pair-Programming, Peer Code Reviews, Mentoring, Teamwork, Initiative, Grit, Resourcefulness
-    Experienced Network Engineer with a demonstrated history of working in the broadcast media industry. Skilled in Active Directory, Windows Server, Networking, Team Leadership, Cisco Certified Network Association (CCNA), ITIL 4, and Oracle Cloud Infrastructure 2019 Certified Architect Associate. Strong information technology professional with a Bachelor's degree focused in Computer Science from Kabul education university.
-    I am passionate about learning new technologies as well as building things.
-    Always looking for new opportunities. 
-    If you'd like to get in contact about potential job opportunities, please reach out via email (mrkamin2@gmail.com), and I will respond Promptly!`;
+  const content = `I am a full-stack web developer who graduated from Microverse, 
+  which is a remote development school that uses pair programming and real-world projects to teach development. 
+  Holding a bachelor's degree in computer science. With 2 years of experience in JavaScript, Ruby, HTML, CSS,
+   ReactJS, NextJS, NodeJS, Redux, Ruby on Rails, Bootstrap, Tailwind, Postgres I have a demonstrated history 
+   of crafting responsive and efficient web applications... 
+   My Github Profile Link: | https://github.com/mrkamin | My Email Address: | mrkamin2@gmail.com`;
 
   const truncatedContent = isFullContentVisible
     ? content
-    : content.slice(0, 518); // Adjust the character limit as needed
+    : content.slice(0, 460); // Adjust the character limit as needed
 
   return (
     <section id="about">
@@ -67,12 +53,12 @@ const About = () => {
           <p>
             {truncatedContent}
             {content.length > 200 && !isFullContentVisible && (
-              <button type="button" onClick={toggleContentVisibility}>
+              <button type="button" className="btn btn-primary" onClick={toggleContentVisibility}>
                 Read More
               </button>
             )}
             {content.length > 200 && isFullContentVisible && (
-              <button type="button" onClick={toggleContentVisibility}>
+              <button type="button" className="btn btn-primary" onClick={toggleContentVisibility}>
                 Read Less
               </button>
             )}
