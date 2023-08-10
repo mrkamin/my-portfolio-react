c6fae871c624a24cc297c58f5a2a050ff1b7ee23
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-08-10 21:51:56 +0500

    Bring some Changes to the Experience component

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,8 @@ const App = () => (
     <Header />
     <Nav />
     <Portfolio />
-    <About />
     <Experince />
+    <About />
     <Services />
     <Testimonials />
     <Contact />
--- src/component/experince/Experince.jsx
+++ src/component/experince/Experince.jsx
@@ -4,103 +4,121 @@ import { BsPatchCheckFill } from 'react-icons/bs';
 
 const Experince = () => (
   <section id="experince">
-    <h5>What Skills I have</h5>
+    <h5>What I know</h5>
     <h2>My Experience</h2>
     <div className="container experience__container">
       <div className="experience__frontend">
-        <h3>Frontend Development</h3>
+        <h3>Languages</h3>
         <div className="experience__content">
-          <article className="experince__details">
-            <BsPatchCheckFill className="experience__details-icon" />
-            <div>
-              <h4>HTML</h4>
-              <small className="text-light">Experienced</small>
-            </div>
-          </article>
-          <article className="experince__details">
-            <BsPatchCheckFill className="experience__details-icon" />
-            <div>
-              {' '}
-              <h4>Css</h4>
-              <small className="text-light">Experienced</small>
-            </div>
-          </article>
-          <article className="experince__details">
-            <BsPatchCheckFill className="experience__details-icon" />
-            <div>
-              <h4>JavaScript</h4>
-              <small className="text-light">Experienced</small>
-            </div>
-          </article>
-          <article className="experince__details">
-            <BsPatchCheckFill className="experience__details-icon" />
-            <div>
-              <h4>Bootstrap</h4>
-              <small className="text-light">Experienced</small>
-            </div>
-          </article>
-          <article className="experince__details">
-            <BsPatchCheckFill className="experience__details-icon" />
-            <div>
-              <h4>Tailwind</h4>
-              <small className="text-light">Experienced</small>
-            </div>
-          </article>
-          <article className="experince__details">
-            <BsPatchCheckFill className="experience__details-icon" />
-            <div>
-              <h4>React</h4>
-              <small className="text-light">Experienced</small>
-            </div>
-          </article>
-          <article className="experince__details">
-            <BsPatchCheckFill className="experience__details-icon" />
-            <div>
-              <h4>Redux</h4>
-              <small className="text-light">Experienced</small>
-            </div>
-          </article>
-        </div>
-      </div>
-      <div className="experience__backend">
-        <h3>Backend Development</h3>
-        <div className="experience__content">
-          <article className="experince__details">
-            <BsPatchCheckFill className="experience__details-icon" />
-            <div>
-              <h4>PostgreSql</h4>
-              <small className="text-light">Experienced</small>
-            </div>
-          </article>
-          <article className="experince__details">
+          <div className="experince__details">
             <BsPatchCheckFill className="experience__details-icon" />
             <div>
               <h4>Ruby</h4>
-              <small className="text-light">Experienced</small>
             </div>
-          </article>
-          <article className="experince__details">
-            <BsPatchCheckFill className="experience__details-icon" />
-            <div>
-              <h4>Ruby on Rails</h4>
-              <small className="text-light">Experienced</small>
-            </div>
-          </article>
-          <article className="experince__details">
-            <BsPatchCheckFill className="experience__details-icon" />
-            <div>
-              <h4>Node JS</h4>
-              <small className="text-light">Experienced</small>
-            </div>
-          </article>
-          <article className="experince__details">
+          </div>
+          <div className="experince__details">
             <BsPatchCheckFill className="experience__details-icon" />
             <div>
               {' '}
-              <h4>PHP</h4>
-              <small className="text-light">Experienced</small>
+              <h4>JavaScript</h4>
             </div>
-          </article>
+          </div>
+          <div className="experince__details">
+            <BsPatchCheckFill className="experience__details-icon" />
+            <div>
+              <h4>HTML</h4>
+            </div>
+          </div>
+          <div className="experince__details">
+            <BsPatchCheckFill className="experience__details-icon" />
+            <div>
+              <h4>CSS</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="experience__frontend">
+        <h3>Frameworks</h3>
+        <div className="experience__content">
+          <div className="experince__details">
+            <BsPatchCheckFill className="experience__details-icon" />
+            <div>
+              <h4>Ruby on Rails</h4>
+            </div>
+          </div>
+          <div className="experince__details">
+            <BsPatchCheckFill className="experience__details-icon" />
+            <div>
+              {' '}
+              <h4>React</h4>
+            </div>
+          </div>
+          <div className="experince__details">
+            <BsPatchCheckFill className="experience__details-icon" />
+            <div>
+              <h4>RSpec</h4>
+            </div>
+          </div>
+          <div className="experince__details">
+            <BsPatchCheckFill className="experience__details-icon" />
+            <div>
+              <h4>Capybara</h4>
+            </div>
+          </div>
+          <div className="experince__details">
+            <BsPatchCheckFill className="experience__details-icon" />
+            <div>
+              <h4>Tailwind</h4>
+            </div>
+          </div>
+          <div className="experince__details">
+            <BsPatchCheckFill className="experience__details-icon" />
+            <div>
+              <h4>Bootstrap</h4>
+            </div>
+          </div>
+          <div className="experince__details">
+            <BsPatchCheckFill className="experience__details-icon" />
+            <div>
+              <h4>Sass</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="experience__backend">
+        <h3>Skills</h3>
+        <div className="experience__content">
+          <div className="experince__details">
+            <BsPatchCheckFill className="experience__details-icon" />
+            <div>
+              <h4>PostgreSql</h4>
+            </div>
+          </div>
+          <div className="experince__details">
+            <BsPatchCheckFill className="experience__details-icon" />
+            <div>
+              <h4>Github</h4>
+            </div>
+          </div>
+          <div className="experince__details">
+            <BsPatchCheckFill className="experience__details-icon" />
+            <div>
+              <h4>Git</h4>
+            </div>
+          </div>
+          <div className="experince__details">
+            <BsPatchCheckFill className="experience__details-icon" />
+            <div>
+              <h4>Codepen</h4>
+            </div>
+          </div>
+          <div className="experince__details">
+            <BsPatchCheckFill className="experience__details-icon" />
+            <div>
+              {' '}
+              <h4>Gitlab</h4>
+            </div>
+          </div>
         </div>
       </div>
     </div>
