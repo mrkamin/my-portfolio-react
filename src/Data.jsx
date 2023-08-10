e42866ba13176d52cfd00d258d9adf5d82d541a0
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-08-10 20:26:25 +0500

    Change layout of Portfolio component

---
+++ src/Data.jsx
@@ -0,0 +1,107 @@
+import Img1 from './assets/todo.jpg';
+import Img2 from './assets/htmlcsscapeston.jpg';
+import Img3 from './assets/reactcapeston.jpg';
+import Img4 from './assets/culculatorjs.jpg';
+import Img5 from './assets/paifj.JPG';
+import Img6 from './assets/mathmagican.JPG';
+import Img7 from './assets/docterapp.JPG';
+import Img8 from './assets/foodrecipe.JPG';
+import Img9 from './assets/spacetravelers.JPG';
+import Img10 from './assets/JustCold.JPG';
+
+const data = [
+  {
+    id: 1,
+    image: Img7,
+    title: 'Doctor Appointment',
+    description: 'A todo list is a practical tool for organizing tasks. It allows users to input, prioritize, and track activities, enhancing productivity and goal management in various aspects of life.',
+    tech: 'JavaScript | React | Ruby on Rails | Postgresql | HTML | CSS',
+    github: 'https://github.com/Iqbal-Elham/Doctor-Reservation-frontend',
+    demo: 'https://reserv-appoinment.onrender.com/',
+  },
+  {
+    id: 2,
+    image: Img8,
+    title: 'Food Recipe',
+    description: 'The JavaScript capstone project is about building a web application based on an external API. The webapp will have 2 or 3 user interfaces. user can see the list of foods and is able to put comments and likes to the each food.',
+    tech: 'JavaScript | Webpack | HTML | CSS',
+    github: 'https://github.com/mrkamin/JavaScript-capstone-project',
+    demo: 'https://mrkamin.github.io/JavaScript-capstone-project/dist/',
+  },
+  {
+    id: 3,
+    image: Img9,
+    title: 'Space Travelers Hub',
+    description: 'This is a React WebApp with real live data from the SpaceX API. The application will allow users to book rockets and join selected space missions. Built with JavaScript, ReactJS, Redux and CSS3',
+    tech: 'JavaScript | React | HTML | CSS',
+    github: 'https://github.com/anthonymr/react-redux-group-project',
+    demo: 'https://sp-axbh.onrender.com/',
+  },
+  {
+    id: 4,
+    image: Img10,
+    title: 'JustCold E-commerce App Product list',
+    description: 'I developed JustCold, an E-commerce app, using React for the frontend and Ruby on Rails for the backend. It enables clients to browse and purchase products seamlessly. Explore the app at https://justcold.onrender.com/.',
+    tech: 'JavaScript | React | Ruby on Rails | Postgresql | HTML | CSS',
+    github: 'https://github.com/mrkamin/JustCold-Frontend',
+    demo: 'https://justcold.onrender.com/',
+  },
+  {
+    id: 10,
+    image: Img1,
+    title: 'To-Do list',
+    description: 'A todo list is a practical tool for organizing tasks. It allows users to input, prioritize, and track activities, enhancing productivity and goal management in various aspects of life.',
+    tech: 'JavaScript | Webpack | HTML | CSS',
+    github: 'https://github.com/mrkamin/ToDoList',
+    demo: 'https://mrkamin.github.io/ToDoList/dist/',
+  },
+  {
+    id: 11,
+    image: Img2,
+    title: 'CONFERENCE',
+    description: 'Discover the UNICEF Annual Human Support Conference through our dedicated website. Stay informed about conference dates, program details, esteemed speakers, partners, and insights into UNICEF mission. Join us in advancing human support and global well-being.',
+    tech: 'JavaScript | HTML | CSS',
+    github: 'https://github.com/mrkamin/Capeston-Project',
+    demo: 'https://mrkamin.github.io/Capeston-Project/',
+  },
+  {
+    id: 12,
+    image: Img3,
+    title: 'Stock Screener',
+    description: 'This capstone project I am going to build is based on an online website for a Stock Screener.This App is a tool quickly find current stock prices in USA companies...In this Repo (Metrics-webapp) 😊 I made the two Components (The home Component. The Details Component).👌',
+    tech: 'JavaScript | React | HTML | CSS',
+    github: 'https://github.com/mrkamin/Metrics-webapp',
+    demo: 'https://superb-puppy-322d9f.netlify.app/',
+  },
+
+  {
+    id: 13,
+    image: Img4,
+    title: ' Calculater',
+    description: 'Calculater is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to: Make simple calculations. Read a random math-related quote.',
+    tech: 'JavaScript | HTML | CSS',
+    github: 'https://github.com/mrkamin/calculator-JavaScript/tree/Dev',
+    demo: 'https://mrkamin.github.io/calculator-JavaScript/',
+  },
+
+  {
+    id: 14,
+    image: Img5,
+    title: 'PAIFJ',
+    description: 'PAIFJ i an online news websit, Stay informed with our comprehensive news website. Explore breaking stories, in-depth articles, and diverse perspectives on global events, technology, culture, and more. Your go-to source for staying connected and up-to-date.',
+    tech: 'JavaScript | React | HTML | CSS',
+    github: 'https://github.com/mrkamin/PAIFJ',
+    demo: 'https://paifj.netlify.app/',
+  },
+  {
+    id: 15,
+    image: Img6,
+    title: 'Math-Magican',
+    description: 'Math magicians is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to: Make simple calculations. Read a random math-related quote.',
+    tech: 'JavaScript | React | HTML | CSS',
+    github: 'https://github.com/mrkamin/math-magican',
+    demo: 'https://musical-monstera-fb67ff.netlify.app/',
+  },
+];
+
+export default data;
